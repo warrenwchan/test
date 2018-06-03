e3e46ea2dd3466e3333f376cfb84ea93ed3ba32c
Warren Chan <warrenchan@warren-2.local>
2018-06-02 20:11:52 -0700

    form config.

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+    <form name="contact" method="POST" netlify>
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role: <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+)
+
+export default IndexPage
